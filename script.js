4b3331c2e50000600f09d794677d9e396fb1f74e
kshwi <kshwiss@gmail.com>
2023-03-01 18:03:18 -0600

    troubleshooting element.innerHTML

--- script.js
+++ script.js
@@ -57,106 +57,10 @@ function writePassword() {
     var Randomer = Math.floor(Math.random() * password.length);
     FinalPass += password.substring(Randomer, Randomer + 1);
   }
-  //My first attempt through, spammed out variables and conditionals. The next day I tested the logic and found errors so restarted simpler
-  /*var NumbersS = "0123456789!@#$%^&*()";
-  var NumbersSU = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var NumbersSUL =
-    "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  var NumbersL = "0123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
-  var NumbersSL = "0123456789abcdefghijklmnopqrstuvwxyz";
-  var NumbersU = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var NumbersLU =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-  var SpecialU = "!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var SpecialL = "!@#$%^&*()abcdefghijklmnopqrstuvwxyz";
-  var SpecialLU =
-    "!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  var UpperL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-*/
-  /*var passwordText = document.querySelector("#password");
-    passwordText.value = password;*/
-  /*
-  if (FirstPrompt && SecondPrompt && ThirdPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer = Math.floor(Math.random() * NumbersSUL.length);
-      password += NumbersSUL.substring(Randomer, Randomer + 1);
-    }
-  } else if (FirstPrompt && SecondPrompt && ThirdPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer2 = Math.floor(Math.random() * NumbersLU.length);
-      password += NumbersLU.substring(Randomer2, Randomer2 + 1);
-    }
-  } else if (FirstPrompt && SecondPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer3 = Math.floor(Math.random() * UpperL.length);
-      password += UpperL.substring(Randomer3, Randomer3 + 1);
-    }
-  } else if (FirstPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer4 = Math.floor(Math.random() * UpperL.length);
-      password += UpperL.substring(Randomer4, Randomer4 + 1);
-    }
-  } else if (SecondPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer5 = Math.floor(Math.random() * Uppers.length);
-      password += Uppers.substring(Randomer5, Randomer5 + 1);
-    }
-  } else if (ThirdPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer6 = Math.floor(Math.random() * Numbers.length);
-      password += Numbers.substring(Randomer6, Randomer6 + 1);
-    }
-  } else if (FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer7 = Math.floor(Math.random() * Special.length);
-      password += Special.substring(Randomer7, Randomer7 + 1);
-    }
-  } else if (FirstPrompt && ThirdPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer8 = Math.floor(Math.random() * NumbersL.length);
-      password += NumbersL.substring(Randomer8, Randomer8 + 1);
-    }
-  } else if (FirstPrompt && SecondPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer99 = Math.floor(Math.random() * SpecialLU.length);
-      password += SpecialLU.substring(Randomer99, Randomer99 + 1);
-    }
-  } else if (FirstPrompt && ThirdPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer9 = Math.floor(Math.random() * NumbersSL.length);
-      password += NumbersSL.substring(Randomer9, Randomer9 + 1);
-    }
-  } else if (SecondPrompt && ThirdPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer10 = Math.floor(Math.random() * NumbersSU.length);
-      password += NumbersSU.substring(Randomer10, Randomer10 + 1);
-    }
-  } else if (ThirdPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer11 = Math.floor(Math.random() * NumbersS.length);
-      password += NumbersS.substring(Randomer11, Randomer11 + 1);
-    }
-  } else if (SecondPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer12 = Math.floor(Math.random() * SpecialU.length);
-      password += SpecialU.substring(Randomer12, Randomer12 + 1);
-    }
-  } else if (FirstPrompt && FourthPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer13 = Math.floor(Math.random() * SpecialL.length);
-      password += SpecialL.substring(Randomer13, Randomer13 + 1);
-    }
-  } else if (SecondPrompt && ThirdPrompt) {
-    for (var i = 0; i <= LengthPrompt; i++) {
-      var Randomer14 = Math.floor(Math.random() * NumbersU.length);
-      password += NumbersU.substring(Randomer13, Randomer14 + 1);
-    }
-  }
-*/
   window.alert("Your new password is " + FinalPass);
-  // Add event listener to generate button
-  password.setAttribute("text", "Test");
+
+  document.password.innerHTML = FinalPass;
 }
+
 generateBtn.addEventListener("click", writePassword);
